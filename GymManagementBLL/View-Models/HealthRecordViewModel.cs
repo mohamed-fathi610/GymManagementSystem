89fc5134d442fdf75840db74030815f66a3e3201
محمد 31157324 <UG_31157324@f-eng.tanta.edu.eg>
2025-10-12 03:58:14 +0300

    App Validation on Create Member View Model

---
+++ GymManagementBLL/View-Models/HealthRecordViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.View_Models
+{
+    internal class HealthRecordViewModel
+    {
+        [Required(ErrorMessage = "Height is Required")]
+        [Range(0.1, 300, ErrorMessage = "Height between 0.1 and 300 cm")]
+        public decimal Height { get; set; }
+
+        [Required(ErrorMessage = "Weight is Required")]
+        [Range(1, 350, ErrorMessage = "Weight between 1 and 350 kg")]
+        public decimal Weight { get; set; }
+
+        [Required(ErrorMessage = "BloodType is Required")]
+        [StringLength(3, ErrorMessage = "Blood Type is max 3")]
+        public string BloodType { get; set; } = null!;
+
+        public string? Note { get; set; }
+    }
+}
