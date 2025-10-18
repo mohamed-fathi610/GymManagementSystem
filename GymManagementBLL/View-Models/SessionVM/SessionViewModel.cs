4df87b6303809b940bdf2871d7efe2404b93fded
محمد 31157324 <UG_31157324@f-eng.tanta.edu.eg>
2025-10-18 06:19:07 +0300

    Session View Model and Computed properties

---
+++ GymManagementBLL/View-Models/SessionVM/SessionViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.View_Models.SessionVM
+{
+    public class SessionViewModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = null!;
+        public int Capacity { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string TrainerName { get; set; } = null!;
+        public string CategoryName { get; set; } = null!;
+
+        public int AvailableSlots { get; set; }
+
+        #region Computed properties
+        public string DateDisplay => $"{StartDate: MMM dd,yyyy}";
+        public string TimeRangeDisplay => $"{StartDate:hh:mm tt} - {EndDate:hh:mm tt}";
+        public TimeSpan Duration => EndDate - StartDate;
+
+        public string Status
+        {
+            get
+            {
+                var now = DateTime.Now;
+                if (now < StartDate)
+                {
+                    return "Upcoming";
+                }
+                else if (now >= StartDate && now <= EndDate)
+                {
+                    return "Ongoing";
+                }
+                else
+                {
+                    return "Completed";
+                }
+            }
+        }
+
+        #endregion
+    }
+}
