b93dfcedf9d26a569181a657cbfe1f607817f4ad
محمد 31157324 <UG_31157324@f-eng.tanta.edu.eg>
2025-10-26 22:23:54 +0300

    Edit Plan

--- GymManagementPL/Controllers/PlanController.cs
+++ GymManagementPL/Controllers/PlanController.cs
@@ -1,4 +1,5 @@
 ﻿using GymManagementBLL.BusinessServices.Interfaces;
+using GymManagementBLL.View_Models.PlanVM;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagementPL.Controllers
@@ -35,6 +36,50 @@ namespace GymManagementPL.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(plan);
+        }
+        #endregion
+
+        #region Edit plan
+
+        public ActionResult Edit(int id)
+        {
+            if (id <= 0)
+            {
+                TempData["ErrorMessage"] = "Id cannot be negative or zero";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var plan = _planService.GetPlanToUpdate(id);
+
+            if (plan == null)
+            {
+                TempData["ErrorMessage"] = "Plan Not Found";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(plan);
+        }
+
+        [HttpPost]
+        public ActionResult Edit([FromRoute] int id, PlanToUpdateViewModel updatedPlan)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("WrongData", "Check Data Validation");
+                return View(updatedPlan);
+            }
+
+            var result = _planService.UpdatePlan(id, updatedPlan);
+
+            if (!result)
+            {
+                TempData["ErrorMessage"] = "Failed to update plan. Please try again.";
+            }
+            else
+            {
+                TempData["SuccessMessage"] = "Plan updated successfully.";
+            }
+            return RedirectToAction(nameof(Index));
         }
         #endregion
     }
