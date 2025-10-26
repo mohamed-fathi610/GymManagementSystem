af18ecb8244b5d2fa5b44155f200b90eff378ecd
محمد 31157324 <UG_31157324@f-eng.tanta.edu.eg>
2025-10-26 21:48:17 +0300

    Get All Plans

---
+++ GymManagementPL/Controllers/PlanController.cs
@@ -0,0 +1,23 @@
+﻿using GymManagementBLL.BusinessServices.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagementPL.Controllers
+{
+    public class PlanController : Controller
+    {
+        private readonly IPlanService _planService;
+
+        public PlanController(IPlanService planService)
+        {
+            _planService = planService;
+        }
+
+        #region Get All Plans
+        public IActionResult Index()
+        {
+            var plans = _planService.GetAllPlans();
+            return View(plans);
+        }
+        #endregion
+    }
+}
